37e8039a49d8ad3398c9c56d64f0637c9b6ca572
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-05-12 13:31:11 +0000

    update fmt

--- src/equilibrium/rulesengine/__init__.py
+++ src/equilibrium/rulesengine/__init__.py
@@ -3,13 +3,12 @@ Provides a generic rules engine.
 """
 
 from equilibrium.rulesengine.Cache import Cache
+from equilibrium.rulesengine.errors import MultipleMatchingRulesError, NoMatchingRulesError, RuleResolveError
 from equilibrium.rulesengine.Executor import Executor
 from equilibrium.rulesengine.Params import Params
 from equilibrium.rulesengine.Rule import Rule, rule
 from equilibrium.rulesengine.RulesEngine import RulesEngine, get
 from equilibrium.rulesengine.RulesGraph import RulesGraph
-
-from .errors import MultipleMatchingRulesError, NoMatchingRulesError, RuleResolveError
 
 __all__ = [
     "Cache",
